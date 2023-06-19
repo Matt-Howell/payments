941ab11386aa5d30ac6878135674f070041e74fe
Matt-Howell <73589713+Matt-Howell@users.noreply.github.com>
2023-06-19 21:11:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -135,4 +135,52 @@ app.get('/history', async (request, response) => {
   response.send(successful)
 });
 
+app.get('/add-credits', async (request, response) => {
+  response.set('Access-Control-Allow-Origin', 'https://keywordcatcher.com')
+  //response.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+
+  let toAdd = request.query.number
+  let userEmail = request.query.email
+
+  const { data: { users }, error: errorB } = await supabase.auth.admin.listUsers({
+    page: 1,
+    perPage: 1000
+  })
+
+  let userID = users.filter(u => u.email == userEmail)[0].id
+
+  const { data: userA, error: errorA } = await supabase.auth.admin.getUserById(
+    userID
+  )
+
+  const { data: user, error } = await supabase.auth.admin.updateUserById(
+    userID,
+    { user_metadata: { credits: parseInt(userA.user.user_metadata.credits + parseInt(toAdd)) } }
+  )
+
+  response.send({credits:user.user.user_metadata.credits})
+});
+
+app.get('/get-users', async (request, response) => {
+  response.set('Access-Control-Allow-Origin', 'https://keywordcatcher.com')
+  // response.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+
+  let type = request.query.type
+
+  const { data: { users }, error: errorB } = await supabase.auth.admin.listUsers({
+    page: 1,
+    perPage: 1000
+  })
+
+  let resultSend = []
+
+  if (type == "all") {
+    resultSend = [...users]
+  } else {
+    resultSend = [...users.filter(u => u.user_metadata.credits > 150)]
+  }
+
+  response.send(resultSend)
+});
+
 app.listen(8080, () => console.log('Running on port 8080'));
